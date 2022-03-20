383f52605927ae1e7eccb29afaa401334e38af38
Logan Thomas <logan.thomas@CO-VAN-MAC103.firmglobal.com>
2022-03-20 15:13:16 -0700

    finished up the technologies portion for now

--- Components/Projects/components/Project.tsx
+++ Components/Projects/components/Project.tsx
@@ -1,7 +1,51 @@
+import { ParallaxLayer } from "@react-spring/parallax";
+import Image from "next/image";
 import React from "react";
+import { useThemeContext } from "../../context/ThemeContext";
+import styles from "../../Technologies/style/Technology.module.scss";
 
-function Project() {
-  return <div>Project</div>;
+interface IProject {
+  title: string;
+  text?: string;
+  scroll?: (to: number) => void;
+  offset: number;
+  image?: any;
+}
+
+function Project(props: IProject) {
+  const { title, scroll, text, offset, image } = props;
+  const { color } = useThemeContext();
+  return (
+    <>
+      <ParallaxLayer offset={offset}>
+        <div className={styles.technology}>{/* <img src={image} /> */}</div>
+      </ParallaxLayer>
+      <ParallaxLayer
+        style={{
+          backgroundColor: offset % 2 === 0 ? color.primary : color.background,
+          //   backgroundImage: `url(${image})`,
+          //   backgroundBlendMode: "overlay",
+          //   backgroundPosition: "center",
+          //   backgroundSize: "contain",
+          //   backgroundRepeat: "repeat",
+        }}
+        offset={offset}
+      >
+        <div className={styles.technology}>
+          <img src={image} />
+          <p>{text}</p>
+          <div
+            style={{
+              color: offset % 2 === 0 ? color.background : color.primary,
+            }}
+            onClick={() => scroll && scroll(offset + 1)}
+          >
+            &rarr;
+          </div>
+        </div>
+      </ParallaxLayer>
+    </>
+  );
 }
 
 export default Project;
