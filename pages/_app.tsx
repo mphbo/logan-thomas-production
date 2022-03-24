f74dadbd0881f03a916af8b9e2dfc270b8cc2314
Logan Thomas <logan.thomas@CO-VAN-MAC103.firmglobal.com>
2022-03-24 11:11:40 -0700

    fixed broken responsiveness in projects

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import { ThemeWrapper } from "../Components/context/ThemeContext";
+import { ThemeWrapper } from "../components/context/ThemeContext";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
