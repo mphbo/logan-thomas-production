0b8ffef674e062da3d7f7a29479aaf67b91ee2e2
Logan Thomas <logan.thomas@CO-VAN-MAC103.firmglobal.com>
2022-03-24 11:08:11 -0700

    fixed broken responsiveness in projects

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
-import MainPageScroll from "../Components/MainPageScroll/";
+import MainPageScroll from "../components/MainPageScroll";
 
 const Home: NextPage = () => {
   return (
